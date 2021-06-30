b18147a495dcb6485f3982c838dd9a838dacaef0
dbere3in <86655136+dbere3in@users.noreply.github.com>
2021-06-30 11:50:15 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,13 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+text = input("Text: ")
+
+i=0
+c=0
+while i<len(text):
+    if text[i] == "a":
+        c+=1
+    i+=1
+
+print(c, "симвлов в строке")
