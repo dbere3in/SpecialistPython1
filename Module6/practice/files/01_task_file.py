dcd7c04c576857eca9efecee16b2171cda919831
dbere3in <86655136+dbere3in@users.noreply.github.com>
2021-07-02 15:41:10 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,14 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+s=0
+not_space_symb = 0
+with open("data/limericks.txt","r",encoding="utf-8") as f:
+    for line in f:
+        for el in line:
+            if el not in (" ", "\n","\t"):
+                not_space_symb+=1
+        if line == "\n": s+=1
+        print(line.rstrip())
+print("\nnot_space_symb: ",not_space_symb)
+print("s: ",s+1)
